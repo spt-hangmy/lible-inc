c36d0c230c553ae915db06b679de5ace18fdbf9f
hangnhm <nguyenhoangmyhang1209@gmail.com>
2024-01-24 16:42:39 +0700

    cosmetics

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $(".wrap-slider").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 3,
+    dots: true,
+    centerMode: true,
+    arrows: true,
+    variableWidth: true,
+  });
+});
